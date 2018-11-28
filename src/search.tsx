7d55f030cae2f771c772a48609086b2d9e872842
Michał Knapik <michal.knapik@u2i.com>
2018-11-28 12:45:48 +0100

    Remove implementation
    To allow individual solutions.

--- src/search.tsx
+++ src/search.tsx
@@ -1,14 +1,11 @@
 import * as React from 'react'
-import { from, Subject } from 'rxjs'
 import axios from 'axios'
-import { debounceTime, distinctUntilChanged, filter, map, mergeMap } from 'rxjs/internal/operators'
 
 interface Props {
   name: string
 }
 
 class Search extends React.Component<Props> {
-  event$: Subject<string>
   state: {
     results: string[],
     error?: string
@@ -18,47 +15,31 @@ class Search extends React.Component<Props> {
   constructor (props: Props) {
     super(props)
 
-    this.event$ = new Subject<string>()
     this.state = {
       results: []
     }
 
     this.onChange = (searchTerm: string) => {
-      this.event$.next(searchTerm)
+      axios
+        .get('https://en.wikipedia.org/w/api.php', {
+          params: {
+            action: 'opensearch',
+            format: 'json',
+            search: searchTerm,
+            origin: '*',
+          },
+        })
+        .then(({data: [, results, ]}) => {
+          this.setState({
+            results
+          })
+        }, error => {
+          this.setState({
+            results: [],
+            error: error,
+          })
+        })
     }
-
-    this.event$.pipe(
-      filter(term => term.length > 2),
-      debounceTime(750),
-      distinctUntilChanged(),
-      mergeMap(term =>
-        from(axios
-          .get('https://en.wikipedia.org/w/api.php', {
-            params: {
-              action: 'opensearch',
-              format: 'json',
-              search: term,
-              origin: '*',
-            },
-          })).pipe(
-          map(response => response.data),
-        ),
-      ),
-    ).subscribe(
-      ([, articles]) => {
-        console.log(articles)
-        this.setState({
-          results: articles
-        })
-      },
-      error => {
-        console.error('error', error)
-        this.setState({
-          results: [],
-          error: error,
-        })
-      },
-    )
   }
 
   render () {
